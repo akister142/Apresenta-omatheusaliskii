f121b7e2a1f8dd4711e236af6a62509c93cc8de9
akister142 <133674087+akister142@users.noreply.github.com>
2024-09-12 01:24:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const sobrenome = 'Aliski';
+const primeiroNome = 'Matheus';
+const idade = 17;
+const altura = 1.81;
+const cidadeNatal = 'São José';
+const peso = '68'
+
+console.log("Meu primeiro nome é;")
+console.log(primeiroNome);
+console.log("Meu segundo nome é;")
+console.log(sobrenome);
+console.log('minha idade é;')
+console.log(idade);
+console.log("Minha altura é:")
+console.log(altura);
+console.log('Meu peso é:')
+console.log(peso);
+console.log('Minha cidade natal é:')
+console.log(cidadeNatal);
